f52406c67f3cb93456a2229eafbc0b084a69ca31
AYUSH MEENA <87579312+isiiilent@users.noreply.github.com>
2022-04-15 11:02:54 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,10 @@
+//Q2
+
+#include <stdio.h>
+int main(){
+    int length,width,height,surfacearea;
+    scanf("%d %d %d", &length,&width,&height);
+    surfacearea=2*((width*length)+(length*height)+(height*width));
+    printf("%d",surfacearea);
+	return 0;
+}
