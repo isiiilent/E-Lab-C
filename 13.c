e3929138c48d81c52ce4cda847dc44b586b8731f
AYUSH MEENA <87579312+isiiilent@users.noreply.github.com>
2022-04-21 22:40:16 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char X,Y;
+    scanf("%c %c",&X,&Y);
+    if(X>Y){
+        printf(">");
+    }else if(X==Y){
+        printf("=");
+    }else{
+        printf("<");
+    }
+	return 0;
+}
