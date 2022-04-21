ec468f70c2d9a1f8969efa6c291ab81d622f32e8
AYUSH MEENA <87579312+isiiilent@users.noreply.github.com>
2022-04-21 22:38:30 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float num1,num2;
+    double resnum1,resnum2;
+    scanf("%f",&num1);
+    scanf("%f",&num2);
+    scanf("%lf",&resnum1);
+    scanf("%lf",&resnum2);
+    printf("%1f\n%1f", num1,num2);
+	return 0;
